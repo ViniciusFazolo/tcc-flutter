bc60451d730c4c1d29b64c2097793f3616cf8397
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-11-09 12:01:49 -0300

    fix: adiciona "/api" na url

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
   
-final String apiBaseUrl = Platform.isAndroid ? "http://10.0.2.2:8080" : "http://localhost:8080";
+final String apiBaseUrl = Platform.isAndroid ? "http://10.0.2.2:8080/api" : "http://localhost:8080/api";
   
 Future<Map<String, String>?> findCep(String cep) async {
   if (cep.isEmpty) return null;
